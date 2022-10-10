e1d47ff724fa2f5369023a52ee2190b790d18de1
Goncalo Silva <goncalo.lslv.silva@gmail.com>
2022-10-10 23:59:41 +0100

    Files from the 3rd class added

---
+++ script3/code/bounded-buffer/test_counter.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/msg.h>
+
+
+void increment(){
+    
+}
+
+int main(void){
+    
+}
+
